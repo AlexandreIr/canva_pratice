0a4ebdd6bb99aba869633d62d53807b61305dba6
Alexandre Fernandes <alex.silva250@hotmail.com>
2024-01-16 15:31:35 -0300

    feat: draw function

--- script.js
+++ script.js
@@ -1,8 +1,41 @@
 let currentColor = 'black';
 const screen = document.querySelector('#tela');
 const ctx = screen.getContext('2d');
+let cantDraw = false;
+let sideX;
+let sideY;
 
-const colorClick = (e) =>{
+const draw = (posX, posY) => {
+    let pX= posX - screen.offsetLeft;
+    let pY= posY - screen.offsetTop;
+
+    ctx.beginPath();
+    ctx.lineWidth = 5;
+    ctx.lineJoin = "round";
+    ctx.moveTo(sideX, sideY);
+    ctx.lineTo(pX, pY);
+    ctx.closePath();
+    ctx.strokeStyle = currentColor;
+    ctx.stroke();
+
+    sideX = pX;
+    sideY = pY;
+}
+
+const mouseDownE = (e) => {
+    cantDraw=true;
+    sideX = e.pageX - screen.offsetLeft;
+    sideY = e.pageY - screen.offsetTop;
+}
+const mouseMoveE = (e) => {
+    if(cantDraw) {
+        draw(e.pageX, e.pageY);
+    }
+}
+const mouseUpE = (e) => {
+    cantDraw=false;
+}
+const colorClick = (e) => {
     let color = e.target.getAttribute('data-color');
     if(color!=currentColor){
         e.target.classList.add('active');
@@ -14,4 +47,7 @@ const colorClick = (e) =>{
 }
 
 document.querySelectorAll('.color').forEach(c=>c.addEventListener('click', colorClick));
+screen.addEventListener('mousedown', mouseDownE);
+screen.addEventListener('mousemove', mouseMoveE);
+screen.addEventListener('mouseup', mouseUpE);
 
